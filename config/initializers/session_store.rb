0f4a78da500068e11c27e20e6a1fba5cb5a0d141
graceeunbekim <alsdnwn1003@berkeley.edu>
2014-10-24 05:25:16 -0700

    comman task isnt working properly

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,2 @@
 # Be sure to restart your server when you modify this file.
-
-<<<<<<< HEAD
 Rails.application.config.session_store :cookie_store, key: '_CallistoReinvented_session'
-=======
-Rails.application.config.session_store :cookie_store, key: '_devise_session'
->>>>>>> a82aaa1014d7ac52070014bbd5c2fd5c8abb7b2f
