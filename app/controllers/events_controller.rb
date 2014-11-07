c29d95ad9a1770362470c91556540f537f78ad49
Alexander Neilson <alex_neilson@berkeley.edu>
2014-11-06 16:00:08 -0800

    more events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,37 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
+  
+  #TODO: render json
+  def create_event
+    company_id = params[:company_id]
+    title = params[:title]
+    type = params[:type]
+    info = params[:info]
+    date = params[:date]
+    result = Event.create_event(company_id, title, type, info, date)
+  end
+
+  #TODO: render json
+  def edit_event
+    event_id = params[:event_id]
+    field = params[:field]
+    value = params[:value]
+    result = Event.edit_event(event_id, field, value)
+  end
+
+  #TODO: render json
+  def delete_event
+    event_id = params[:event_id]
+    result = Event.delete_event(event_id)
+  end
+
+  #TODO: render json
+  def get_event
+    event_id = params[:event_id]
+    result = Event.get_event[event_id]
+  end
+
   def index
     @events = Event.all
     respond_with(@events)
