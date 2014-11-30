4c968c4a4ad8c2f7cd8a20ca6841d0ea4db73704
Alex Jin <alexgbjin@gmail.com>
2014-11-29 21:46:18 -0800

    basic rec implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,22 @@ class UsersController < ApplicationController
 
   def create_post
   end
+
+  def initialize_tags
+    tags = current_user[:skill].split(", ")
+    saved_tags = current_user.saved_tags
+    tags.each do |t|
+      saved_tags[t] = { count: 1, weight: 1.0 }    # Add tag into history and initialize counter
+    end
+    current_user.update(saved_tags: saved_tags)
+    render json: {errCode: 1}
+  end
+
+  def fill_tags
+    user_tags = current_user[:saved_tags]
+    render json: {errCode: 1}
+  end
+
+  def reset_tags
+  end
 end
